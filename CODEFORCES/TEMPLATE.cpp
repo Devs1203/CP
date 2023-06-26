9917b82a2c82809e5e2d4bd4df101389870d09af
Devs1203 <128769844+Devs1203@users.noreply.github.com>
2023-06-26 12:21:18 +0530

    Update TEMPLATE.cpp

--- CODEFORCES/TEMPLATE.cpp
+++ CODEFORCES/TEMPLATE.cpp
@@ -3,14 +3,10 @@
 #pragma GCC optimize("Ofast")
 #include<bits/stdc++.h>
 using namespace std;
-#define endl "\n"
 #define ll long long
 #define fast ios_base::sync_with_stdio (false); cin.tie (NULL); cout.tie (NULL)
 
 void solve(){
-    ll a=0,b=0,c=0,d=0,n=0,x=0,y=0,p=0,q=0;
-    string s,ss,s1,s2;
-    vector<int> k() ;
 
 }
 int main(){
